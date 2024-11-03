0203fe43ec9cc7b55f557d7384b9e18ac832dd5d
Watsachol <s6604062630510@email.kmutnb.ac.th>
2024-11-03 07:15:02 +0700

    Fix missing react-scripts

--- src/CallFunc.js
+++ src/CallFunc.js
@@ -10,14 +10,14 @@ export function CallFormula(name, id_fomula) {
     .then((response) => {
         console.log('Response from API:', response.data);
         if (response.data && response.data.formula) {
-            return response.data.formula;  // Return formula if found
+            return response.data.formula; 
         } else {
-            return "No formula found";     // Return message if formula not found
+            return "No formula found";   
         }
     })
     .catch((error) => {
         console.error('Error fetching formula:', error);
-        return "Error occurred";          // Return error message if request fails
+        return "Error occurred"; 
     });
 }
 
